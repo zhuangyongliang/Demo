8630e9717c50cb9a8c8c842a9d1742a2756b35bd
yzhuang <yzhuang@aliyun.com>
2015-06-08 17:08:47 +0800

    First create

---
+++ src/com/ext/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ext.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello World V1");
+	}
+
+}
