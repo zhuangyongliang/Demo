6ef92eb24b4946877f7c0c7b4944d8e5fc4c5940
13823643692 <Leo@think>
2015-06-08 17:25:52 +0800

    add exit

--- src/com/ext/test/HelloWorld.java
+++ src/com/ext/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello World V2");
+       System.exit(0);
 	}
 
 }
